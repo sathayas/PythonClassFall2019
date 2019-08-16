68db671f738fcee2209d83d89e47a59b5ab03e3e
Satoru Hayasaka <hayasaka@utexas.edu>
2019-08-15 22:53:33 -0600

    Burrito example

---
+++ operatorExamples/Burrito.py
@@ -0,0 +1,4 @@
+wallet = float(input('How much money do you have?'))
+burito = float(input('How much is a burito?'))
+numBurito = wallet // burito
+print('You can buy ' + str(int(numBurito)) + ' burito')
