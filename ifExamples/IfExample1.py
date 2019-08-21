f2e3783cfb2399cddc989cd41df33e45787bf505
Satoru Hayasaka <hayasaka@utexas.edu>
2019-08-21 12:58:45 -0500

    If statement example

---
+++ ifExamples/IfExample1.py
@@ -0,0 +1,9 @@
+age = 23
+hand = 'right'
+# evaluating the age requirement
+if age>=18:
+    print('The age requirement is met')
+
+# evaluating the handedness requirement
+if hand=='right':
+    print('The handedness requirement is met')
