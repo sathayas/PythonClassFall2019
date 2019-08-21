bf2a755fb9a56855ea01a93c178ccabb0aa9aa2e
Satoru Hayasaka <hayasaka@utexas.edu>
2019-08-21 13:00:37 -0500

    If statement with boolean operators

---
+++ ifExamples/IfExample2.py
@@ -0,0 +1,20 @@
+# Example of 'and' operator
+age = 23
+hand = 'right'
+# evaluating the age and handedness requirements at once
+if (age>=18) and (hand=='right'):
+    print('The age is ' + str(age) + ' years old.')
+    print('The handedness is ' + hand + '-handed.')
+    print('Meeting all the requirements.')
+
+
+
+
+# Example of 'or' operator
+age = 17
+hand = 'right'
+# evaluating the age or handedness requirements at once
+if (age>=18) and (hand=='right'):
+    print('The age is ' + str(age) + ' years old.')
+    print('The handedness is ' + hand + '-handed.')
+    print('At least one requirement is met.')
