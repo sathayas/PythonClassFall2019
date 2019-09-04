dad119d5f262483efe8d77db2aa48fa230d24b2a
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-04 12:20:36 -0500

    Simple for loop examples

---
+++ forExamples/ForLoopExamples.py
@@ -0,0 +1,9 @@
+# Printing are we there yet 10 times
+for i in range(10):
+    print('Are we there yet?')
+
+
+# printing i inside a for loop
+for i in range(10):
+    print('The value of the variable i is ' + str(i))
+    
