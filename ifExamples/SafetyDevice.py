ab1c8500e7cc828aede6583f4207e834d0ba8767
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-05 14:46:44 -0500

    Safety device exercise

---
+++ ifExamples/SafetyDevice.py
@@ -0,0 +1,4 @@
+weight = float(input('Please enter your weight (pounds): '))
+height = float(input('Please enter your height (inches): '))
+if (weight<=300) and (height<=78):
+    print('Requirements met!')
