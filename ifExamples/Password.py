aa63a0ffade6e5b2d9eee45f9f01c298b356c979
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-05 15:03:04 -0500

    Password exercise

---
+++ ifExamples/Password.py
@@ -0,0 +1,6 @@
+truePassword = 'password'
+userPassword = input('Please enter the password: ')
+if userPassword==truePassword:
+    print('Correct password entered')
+else:
+    print('Invalid password')
