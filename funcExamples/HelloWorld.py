c1b043c2ab49a0f8ee0c67ea1dc12d6486455ef2
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-06 14:45:16 -0500

    Printing out hello world using funcitons

---
+++ funcExamples/HelloWorld.py
@@ -0,0 +1,8 @@
+# function to print out Hello World!
+def HelloWorld():
+    print('Hello World!')
+
+# function to print out Hello World! 110 times
+def HelloWorld10():
+    for i in range(10):
+        HelloWorld()
