fa55ce07cacb1c8480aa22c488cbb8c0e7b4084d
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-09 10:30:35 -0500

    Added an example of returning two parameters

--- funcExamples/TempConversion.py
+++ funcExamples/TempConversion.py
@@ -6,3 +6,13 @@ def FtoC(tempF):
 print(FtoC(-20))
 print(FtoC(0))
 print(FtoC(100))
+
+
+# F to C temperature conversion function, returning both temperatures
+def FtoC_ReturnBoth(tempF):
+    tempC = (tempF - 32) * (5/9)
+    return tempC, tempF
+
+
+C, F = FtoC_ReturnBoth(90)
+print(str(F) +' degree Fahrenheit = '+ str(C) +' degree Celsius')
