c3e97569e40b63a5d486a469f9c45c7af545e253
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-09 10:37:11 -0500

    Local and global variable example

---
+++ funcExamples/PrintAnswer.py
@@ -0,0 +1,8 @@
+def printAnswer(Answer):
+    Answer = Answer -5
+    print('Variable Answer inside the function is ' + str(Answer))
+
+Answer = 42
+print('Variable Answer is ' + str(Answer))
+printAnswer(Answer)
+print('Variable Answer is ' + str(Answer))
