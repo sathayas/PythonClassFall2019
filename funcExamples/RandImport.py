8d158ee21a3390e6d2668bc98dca27a4bc8523a4
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-09 11:04:41 -0500

    Now has a version with import from

--- funcExamples/RandImport.py
+++ funcExamples/RandImport.py
@@ -1,4 +1,9 @@
+# Import using import
 import random
 for i in range(10):
     print(random.randint(1,10))
-    
+
+# Import using from
+from random import randint
+for i in range(10):
+    print(randint(1,10))
