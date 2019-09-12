b2a61c08c85641866295ea1a802658e002bcd1d3
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-12 13:11:47 -0500

    Now added examples of enumerate

--- listExamples/ForLoopWithList.py
+++ listExamples/ForLoopWithList.py
@@ -1,3 +1,21 @@
+# For loop with a numerical list
 minutes = [0, 15, 30, 45, 60, 75, 90, 105, 120]
 for iMin in minutes:
     print('Elapsed time = ' + str(iMin))
+
+# For loop with a string list
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for iMenu in menu:
+    print("Today's special is " + iMenu)
+
+# For loop with a range function
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for i in range(len(day)):
+    print(day[i] + "'s special is " + menu[i])
+
+# For loop with enumerate
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for i, iDay in enumerate(day):
+    print(iDay + "'s special is " + menu[i])
