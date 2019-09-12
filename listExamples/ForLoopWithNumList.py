7a9847a691b55fefc2ce545a3651d37ca0e63587
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-12 13:05:38 -0500

    Added a for loop with a string list

---
+++ listExamples/ForLoopWithNumList.py
@@ -0,0 +1,9 @@
+# For loop with a numerical list
+minutes = [0, 15, 30, 45, 60, 75, 90, 105, 120]
+for iMin in minutes:
+    print('Elapsed time = ' + str(iMin))
+
+# For loop with a string list
+menu = ['Chili', 'Hot Dog', 'Salad', 'Tacos', 'Pizza']
+for iMenu in menu:
+    print("Today's special is " + iMenu)
