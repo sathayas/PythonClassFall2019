182f1f8873b4814fdcd3db18af62cbc556044e7d
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-12 13:02:48 -0500

    For loop with a numerical list

--- listExamples/BandMembers.py
+++
@@ -1,5 +0,0 @@
-memberNames = ['Paul', 'John', 'George', 'Ringo']
-
-print('The band members are:')
-for iName in memberNames:
-    print('   ' + iName)
---
+++ listExamples/ForLoopWithNumList.py
@@ -0,0 +1,3 @@
+minutes = [0, 15, 30, 45, 60, 75, 90, 105, 120]
+for iMin in minutes:
+    print('Elapsed time = ' + str(iMin))
