9f4b5ccf6c6444829ddd683a50287ad33488a351
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-17 12:03:56 -0500

    for loop with items added

--- dictExamples/forLoopDict.py
+++ dictExamples/forLoopDict.py
@@ -4,3 +4,8 @@ respTime = {'congruent':132, 'incongruent':250, 'mixed':189}
 # for loop over keys
 for iKey in respTime.keys():
     print('Condition: ' + iKey)
+
+# for loop over items
+print('Repsponse times:')
+for iKey, iValue in respTime.items():
+    print(str(iValue) + ' (' + iKey + ')')
