92eba34405e98ac2bee62a7aff456f1bf4d170b6
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-19 23:31:18 -0500

    Day of week exercise

---
+++ listExamples/DayOfWeek.py
@@ -0,0 +1,11 @@
+def dayCode(day):
+    shortDay = day[:2]
+    return shortDay
+
+print(dayCode('Sunday'))
+print(dayCode('Monday'))
+print(dayCode('Tuesday'))
+print(dayCode('Wednesday'))
+print(dayCode('Thursday'))
+print(dayCode('Friday'))
+print(dayCode('Saturday'))
