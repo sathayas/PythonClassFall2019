410282be6996da6931f2a9481f75d99526723292
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-24 15:49:05 -0500

    Favorite fruit example

---
+++ stringExamples/FavoriteFruits.py
@@ -0,0 +1,8 @@
+print('What is your favorite fruit?')
+userFruit = input()
+
+if userFruit == 'apple':
+    print('I like ' + userFruit + ' too.')
+else:
+    print('I eat ' + userFruit + ' occasionally')
+
