630c35dc74135b0459412b68390032bde9ec9b3a
Satoru Hayasaka <hayasaka@utexas.edu>
2019-09-26 13:27:04 -0500

    Added another section with appending to an existing file

--- fileExamples/WriteIDs.py
+++ fileExamples/WriteIDs.py
@@ -1,6 +1,14 @@
+# writing to a file
 f = open('IDs.txt','w')
 for i in range(10,26):
     f.write('%05d' % i + '\n')
-    
+
 f.close()
 
+
+# appending to an existing file
+f = open('IDs.txt','a')
+for i in range(110,126):
+    f.write('%05d' % i + '\n')
+
+f.close()
