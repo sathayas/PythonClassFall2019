75fa0b55f07268e3edfaf01adbcd5bdd68430a94
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-23 09:27:27 -0500

    Adjusting title, labels, axes etc.

---
+++ plotExamples/PlotTitleLabels.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generating simple data
+t = np.linspace(0,4*np.math.pi,64)
+y = np.sin(t)
+
+# plot with title and axis labels
+plt.plot(t, y, 'b-')
+plt.title('Sine function')
+plt.ylabel('sin(t)')
+plt.xlabel('Angle t (radian)')
+plt.show()
+
+
+# The range of the axes are controlled now
+plt.plot(t, y, 'b-')
+plt.title('Sine function')
+plt.ylabel('sin(t)')
+plt.xlabel('Angle t (radian)')
+plt.axis([0,13,-2,2])
+plt.show()
+
