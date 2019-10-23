2e5716194d404966dd2501ff072c7f1264277b9c
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-23 11:26:42 -0500

    Saving a figure as a file

---
+++ plotExamples/SaveFigure.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generating simple data
+t = np.linspace(0,4*np.math.pi,64)
+y = np.sin(t)
+
+# The range of the axes are controlled now
+plt.plot(t, y, 'b-')
+plt.title('Sine function')
+plt.ylabel('sin(t)')
+plt.xlabel('Angle t (radian)')
+plt.axis([0,13,-2,2])
+plt.savefig('SinPlot.png', dpi=600)
+plt.show()
+
