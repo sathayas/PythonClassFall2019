3b88a0e7f7d122a36795a691078fdc61b8756440
Satoru Hayasaka <hayasaka@utexas.edu>
2019-10-31 09:46:06 -0500

    Added more comments

--- pandasExamples/FirstDataFrame.py
+++ pandasExamples/FirstDataFrame.py
@@ -11,11 +11,13 @@ condA = np.array([80, 70, 63, 73, 64, 78, 68, 45, 66, 56])
 condB = np.array([ 78,  97, 120,  92,  84, 116,  81,  81, 104,  82])
 condC = np.array([ 75,  49,  58, 115,  97,  95,  93,  66,  84,  67])
 
+# Creating an empty dataframe
 expData = pd.DataFrame()
+
+# adding variable control under the dataframe, whose values are array control
 expData['control'] = control
+
+# adding other variables as well
 expData['condA'] = condA
 expData['condB'] = condB
 expData['condC'] = condC
-
-
-
