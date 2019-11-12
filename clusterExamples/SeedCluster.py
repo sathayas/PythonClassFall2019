5fe5bc1e0489248c4c0234e17cdddb7e22769040
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-12 13:02:01 -0600

    Seed clustering with results plotted in a scatter plot

--- clusterExamples/SeedCluster.py
+++ clusterExamples/SeedCluster.py
@@ -1,28 +1,62 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 # loadin the data
 seedData = pd.read_csv('seeds_dataset.txt', sep='\t', header=None)
 seedFeatures = np.array(seedData.iloc[:,:7])
 seedTargets = np.array(seedData.iloc[:,7])
 targetNames = ['Kama','Rosa','Canadian']
-targetColors = ['red','blue','green']
 
+# feature names
+featureNames = ['area',
+                'perimeter',
+                'compactness',
+                'length of kernel',
+                'width of kernel',
+                'asymmetry coefficient',
+                'length of kernel groove',
+                'class']
+seedData.columns = featureNames
+
+
+# creating a list of colors
+yColor = []
+colorVector = 'rgb'
+for iClass in range(1,4):
+    yColor = yColor + ([colorVector[iClass-1]] * len(seedTargets[seedTargets==iClass]))
+
+# plotting the scatter plot matrix
+pd.plotting.scatter_matrix(seedData.iloc[:,:7], figsize=[9,9], color=yColor)
+plt.show()
+
+
+
+# K-means clustering
 #
-#
-# Code for exercise. Determine 7 PCs in an array named seedPCs
-#
+# EXERCISE: FILL IN THE CODE TO PERFORM K-MEANS CLUSTERING
 #
 
-# plotting the first 2 PCs
-for iTarget in np.arange(1,4):
-    plt.plot(seedPCs[seedTargets==iTarget,0],
-             seedPCs[seedTargets==iTarget,1],
-             marker='.', ls='none',
-             c=targetColors[iTarget-1], label=targetNames[iTarget-1])
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend(loc=4)
+
+### plotting the clusters
+plt.figure(figsize=[8,4])
+xFeature = 6
+yFeature = 5
+# First, results from K-means
+plt.subplot(121)
+plt.scatter(seedFeatures[:,xFeature],
+            seedFeatures[:,yFeature], c=y_clus, marker='+')
+plt.xlabel(featureNames[xFeature])
+plt.ylabel(featureNames[yFeature])
+plt.title('Clusters from K-means')
+
+# As a comparison, the true clusters
+plt.subplot(122)
+plt.scatter(seedFeatures[:,xFeature],
+            seedFeatures[:,yFeature], c=yColor, marker='+')
+plt.xlabel(featureNames[xFeature])
+plt.ylabel(featureNames[yFeature])
+plt.title('True clusters')
+
 plt.show()
