f0ec7e3802adc3cfc9a269aa1435a5d927f6bb60
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-13 15:04:40 -0600

    Scree plot has been added

--- clusterExamples/PersonalityCluster.py
+++ clusterExamples/PersonalityCluster.py
@@ -6,5 +6,21 @@ from sklearn.cluster import KMeans
 
 # loadin the data
 pTraitData = pd.read_csv('personality0.txt', sep=' ')
+numFeatures = len(pTraitData.columns)  # number of features
 
-numFeatures = 32
+
+
+# determinging the number of clusters (up to 30 clusters)
+SSE = []
+nMaxClusters = 30
+for iClus in range(1,nMaxClusters+1):
+    # K-means clustering
+    km = KMeans(n_clusters=iClus)  # K-means with a given number of clusters
+    km.fit(pTraitData)  # fitting the personality data
+    SSE.append(km.inertia_) # recording the sum of square distances
+
+# plotting the sum of square distance
+plt.plot(np.arange(1,nMaxClusters+1),SSE,marker = "o")
+plt.xlabel('Number of clusters')
+plt.ylabel('Sum of sq distances')
+plt.show()
