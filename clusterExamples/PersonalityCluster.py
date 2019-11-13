c3849c85d481f64a04dc5b454c74dadaaf6e9c0a
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-13 15:23:53 -0600

    Added the second k-means; plotting two features; extracting feature names

--- clusterExamples/PersonalityCluster.py
+++ clusterExamples/PersonalityCluster.py
@@ -6,8 +6,7 @@ from sklearn.cluster import KMeans
 
 # loadin the data
 pTraitData = pd.read_csv('personality0.txt', sep=' ')
-numFeatures = len(pTraitData.columns)  # number of features
-
+featureNames = pTraitData.columns  # list of features
 
 
 # determinging the number of clusters (up to 30 clusters)
@@ -23,4 +22,24 @@ for iClus in range(1,nMaxClusters+1):
 plt.plot(np.arange(1,nMaxClusters+1),SSE,marker = "o")
 plt.xlabel('Number of clusters')
 plt.ylabel('Sum of sq distances')
+plt.show()
+
+
+# K-means clustering again
+# We will go with 3 clusters (btw is not the best solution)
+nClus = 3
+km = KMeans(n_clusters=nClus)
+km.fit(pTraitData)  # fitting the principal components
+y_clus = km.labels_   # clustering info resulting from K-means
+
+
+### plotting the clusters
+# with two of the features
+xFeature = 21 # index for the feature on the x-axis
+yFeature = 28 # index for the feature on the y-axis
+plt.scatter(pTraitData.iloc[:,xFeature],
+            pTraitData.iloc[:,yFeature],c=y_clus,marker='+')
+plt.xlabel(featureNames[xFeature])
+plt.ylabel(featureNames[yFeature])
+plt.title(featureNames[xFeature] + ' v.s. ' + featureNames[yFeature])
 plt.show()
