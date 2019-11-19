940ccf7ff1b2deb044be876e0e8bfdaf0aab77ea
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-19 11:51:02 -0600

    SVM exercise added

--- classExamples/IrisSVM.py
+++ classExamples/IrisSVM.py
@@ -11,20 +11,15 @@ y = iris.target
 target_names = iris.target_names
 
 # spliting the data into training and testing data sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=50, 
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=50,
                                                     random_state=0)
 
-# SVM model fitting
-sv = SVC(kernel='linear', C=0.1)
-sv.fit(X_train,y_train)
+# EXERCISE: SVM CLASSIFIER
 
-# SVM classifier
-y_pred = sv.predict(X_test)   # predicted class
 
 # Confusion matrix
 print(confusion_matrix(y_test,y_pred))
 
 # classification report
 print(classification_report(y_test, y_pred, target_names=target_names))
-
