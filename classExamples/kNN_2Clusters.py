c3b921237e615423486e8274548298078407d34a
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-19 10:01:31 -0600

    kNN classifier, 2-cluster toy example

---
+++ classExamples/kNN_2Clusters.py
@@ -0,0 +1,136 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# producing a toy data set: two clusters, separated
+X, y = make_blobs(n_samples=50, centers=2,
+                  random_state=0, cluster_std=1.0)
+
+
+# plotting the toy data plus a new observation
+x_new = 0.5
+y_new = 2.75
+plt.figure(figsize=[6,6])
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.plot(x_new, y_new, marker='x', c='r', ms=15)
+plt.show()
+
+
+# k nearest neighbors
+plt.figure(figsize=[9,3])
+plt.subplot(131)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=10)
+plt.plot(x_new, y_new, marker='x', c='r', ms=8)
+circle = plt.Circle((x_new, y_new), radius=0.7, color='r', linewidth=1,
+                    ls='--', fill=False)
+ax = plt.gca()
+ax.add_artist(circle)
+ax.set_aspect('equal')
+plt.title('k=3')
+
+plt.subplot(132)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=10)
+plt.plot(x_new, y_new, marker='x', c='r', ms=8)
+circle = plt.Circle((x_new, y_new), radius=1.0, color='r', linewidth=1,
+                    ls='--', fill=False)
+ax = plt.gca()
+ax.add_artist(circle)
+ax.set_aspect('equal')
+plt.title('k=5')
+
+plt.subplot(133)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=10)
+plt.plot(x_new, y_new, marker='x', c='r', ms=8)
+circle = plt.Circle((x_new, y_new), radius=1.37, color='r', linewidth=1,
+                    ls='--', fill=False)
+ax = plt.gca()
+ax.add_artist(circle)
+ax.set_aspect('equal')
+plt.title('k=10')
+
+plt.show()
+
+
+# k nearest neighbor classifier object
+kNN5 = KNeighborsClassifier(5, weights='uniform')
+kNN5.fit(X,y)
+
+
+# decision boundary
+from DecisionBoundary import plot_contours
+
+plt.figure(figsize=[6,6])
+ax = plt.subplot(111)
+plot_contours(ax, kNN5, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.show()
+
+
+
+# k nearest neighbor classifier object with different k
+kNN3 = KNeighborsClassifier(3, weights='uniform')
+kNN3.fit(X,y)
+
+kNN10 = KNeighborsClassifier(10, weights='uniform')
+kNN10.fit(X,y)
+
+
+# decision boundary
+plt.figure(figsize=[9,4])
+ax = plt.subplot(131)
+plot_contours(ax, kNN3, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=3')
+
+ax = plt.subplot(132)
+plot_contours(ax, kNN5, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=5')
+
+ax = plt.subplot(133)
+plot_contours(ax, kNN10, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=10')
+
+plt.show()
+
+
+# k nearest neighbor classifier object with different k
+# now weighted by the distance
+kNN3dist = KNeighborsClassifier(3, weights='distance')
+kNN3dist.fit(X,y)
+
+kNN5dist = KNeighborsClassifier(5, weights='distance')
+kNN5dist.fit(X,y)
+
+kNN10dist = KNeighborsClassifier(10, weights='distance')
+kNN10dist.fit(X,y)
+
+
+# decision boundary
+plt.figure(figsize=[9,4])
+ax = plt.subplot(131)
+plot_contours(ax, kNN3dist, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=3')
+
+ax = plt.subplot(132)
+plot_contours(ax, kNN5dist, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=5')
+
+ax = plt.subplot(133)
+plot_contours(ax, kNN10dist, X[:, 0], X[:, 1],
+                  cmap=plt.cm.coolwarm, alpha=0.4)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, s=30)
+plt.title('k=10')
+
+plt.show()
