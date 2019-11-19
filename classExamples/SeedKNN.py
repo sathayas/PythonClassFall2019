277d2e2e30ac071f980793142e2ff9659980e14c
Satoru Hayasaka <hayasaka@utexas.edu>
2019-11-19 10:50:50 -0600

    Seed data kNN classifier

---
+++ classExamples/SeedKNN.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# loadin the data
+seedData = pd.read_csv('seeds_dataset.txt', sep='\t', header=None)
+X = np.array(seedData.iloc[:,:7])   # features (7 variables)
+y = np.array(seedData.iloc[:,7])    # target labels
+targetNames = ['Kama','Rosa','Canadian']
+
+
+# spliting the data into training and testing data sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=70,
+                                                    random_state=1234)
+
+
+
+# Predicted classes
+y_pred = kNN.predict(X_test)
+
+
+# classifier performance
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test, y_pred,
+                            target_names=target_names))
